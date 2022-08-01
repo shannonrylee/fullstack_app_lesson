7d0bede9b3c15164cf7890d7e58e8ebdad593530
Your Actual Name Here <SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04>
2022-08-01 09:26:14 -0400

    server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const routes = require("./routes");
+const db = require("./db");
+const logger = require("morgan");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(logger("dev"));
+
+app.use(cors({ credentials: true, origin: true }));
+
+app.options("*", cors());
+
+app.use("/api", routes);
+
+db.on(
+  "error",
+  console.error.bind(console, "Cabana Day MongoDB connection error:")
+);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
